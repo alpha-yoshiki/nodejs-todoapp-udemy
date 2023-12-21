2ea6846a79e68efe3f085759d435ccee05a954c4
aibara_surface <y.aibara.math95@gmail.com>
2023-12-21 11:34:51 +0900

    db setting start

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("app")
+const express = require("express");
+const app = express()
+
+const taskRoute = require("./routes/tasks")
+
+const port = 3000
+// app.use(express.json());
+app.use("/api/v1/tasks", taskRoute);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+
+app.listen(port, () => {
+  console.log(`サーバーrun on port ${port}`)
+})
+
