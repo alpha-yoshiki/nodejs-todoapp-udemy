77c9b7c244d48292ac96db21cc2acc30b4981d77
aibara_surface <y.aibara.math95@gmail.com>
2023-12-31 05:04:38 +0900

    done API part

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,7 +1,12 @@
 const Task = require("../models/Task")
 
-const getAllTasks = (req, res) => {
-    res.send('タスクを全て取得しました');
+const getAllTasks = async (req, res) => {
+    try {
+        const allTask = await Task.find({});
+        res.status(200).json(allTask);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 };
 
 const createTask = async (req, res) => {
@@ -13,16 +18,48 @@ const createTask = async (req, res) => {
     }
 };
 
-const getSingleTask = (req, res) => {
-    res.send('ある特定のタスクを取得しました');
+const getSingleTask = async (req, res) => {
+    try {
+        const getSingleTask = await Task.findOne({_id: req.params.id});
+        if (!getSingleTask) {
+            return res.status(404).json(`_id:${req.params.id}は存在しません`)
+        }
+        res.status(200).json(getSingleTask);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 };
 
-const updateTask = (req, res) => {
-    res.send('ある特定のタスクを更新しました');
+const updateTask = async (req, res) => {
+    try {
+        const upadateTask = await Task.findOneAndUpdate(
+            {_id: req.params.id},
+            req.body,
+            {
+                new: true
+            }
+            );
+        if (!upadateTask) {
+            return res.status(404).json(`_id:${req.params.id}は存在しません`)
+        }
+        res.status(200).json(upadateTask);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 };
 
-const deleteTask = (req, res) => {
-    res.send('ある特定のタスクを削除しました');
+const deleteTask = async (req, res) => {
+    try {
+        const deleteTask = await Task.findOneAndDelete(
+            {_id: req.params.id}
+            );
+        if (!deleteTask) {
+            return res.status(404).json(`_id:${req.params.id}は存在しません`)
+        }
+        res.status(200).json(deleteTask);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 };
 
 module.exports = {
